53fbe88924209978e3e87ab6e01a7f2ef465f36b
R. Kukuh <rkukuh@gmail.com>
2018-03-31 16:06:48 +0700

    Upgraded to Xcode 9.3 recommendation setting

--- Tebak Angka/ViewController.swift
+++ Tebak Angka/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var userInput: UITextField!
+    
+    @IBAction func tebakDidPressed(_ sender: Any) {
+        
+        let randomNumber = String(arc4random_uniform(9) + 1)
+        
+        if (userInput.text?.isEmpty)! {
+            
+            resultLabel.text = "Masukkan hanya angka 1-10"
+            
+            return
+        }
+        
+        if userInput.text == randomNumber {
+            
+            resultLabel.text = "Tebakanmu benar!"
+            
+        } else {
+            
+            resultLabel.text = "Tebakanmu salah! \nAngka yang benar adalah \(randomNumber)"
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
